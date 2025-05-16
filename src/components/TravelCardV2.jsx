5e14b28f6882f04d74ebcde5b58a0b0a85b8c621
Fabio-Bianco <biancobfabio@gmail.com>
2025-05-17 00:26:57 +0200

    refactor: update TravelCard components with links to trip and guide detail pages

--- src/components/TravelCardV2.jsx
+++ src/components/TravelCardV2.jsx
@@ -1,18 +1,27 @@
-export default function TravelCardV2({ data }) {
+import { Link } from "react-router-dom";
 
-    return <>
-        <div className="col">
-            <div className="card">
-                <div className="card-header">
-                    {data.packageName}
-                </div>
-                <div className="card-body">
-                    <p className="card-text">Destination: {data.destination}</p>
-                    <p className="card-text">Departure: {data.departure}</p>
-                    <p className="card-text">Return: {data.return}</p>
-                    <a href="#" className="btn btn-primary">See more</a>
-                </div>
-            </div>
+// Card con header e link a viaggio + guida
+function TravelCardV2({ data }) {
+  return (
+    <div className="col">
+      <div className="card">
+        <div className="card-header">{data.packageName}</div>
+        <div className="card-body">
+          <p className="card-text">Destination: {data.destination}</p>
+          <p className="card-text">Departure: {data.departure}</p>
+          <p className="card-text">Return: {data.return}</p>
+
+          <Link to={`/trips/${data.id}`} className="btn btn-primary me-2">
+            Vedi viaggio
+          </Link>
+
+          <Link to={`/guides/${data.id}`} className="btn btn-outline-secondary">
+            Guida
+          </Link>
         </div>
-    </>
+      </div>
+    </div>
+  );
 }
+
+export default TravelCardV2;
--- src/components/TravelCardV3.jsx
+++ src/components/TravelCardV3.jsx
@@ -1,16 +1,27 @@
-export default function TravelCardV3({ data }) {
+import { Link } from "react-router-dom";
 
-    return <>
-        <div className="col">
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">{data.packageName}</h5>
-                    <p className="card-text">Destination: {data.destination}</p>
-                    <p className="card-text">Departure: {data.departure}</p>
-                    <p className="card-text">Return: {data.return}</p>
-                    <a href="#" className="btn btn-primary">See more</a>
-                </div>
-            </div>
+// Card semplice con link a viaggio + guida
+function TravelCardV3({ data }) {
+  return (
+    <div className="col">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{data.packageName}</h5>
+          <p className="card-text">Destination: {data.destination}</p>
+          <p className="card-text">Departure: {data.departure}</p>
+          <p className="card-text">Return: {data.return}</p>
+
+          <Link to={`/trips/${data.id}`} className="btn btn-primary me-2">
+            Vedi viaggio
+          </Link>
+
+          <Link to={`/guides/${data.id}`} className="btn btn-outline-secondary">
+            Guida
+          </Link>
         </div>
-    </>
+      </div>
+    </div>
+  );
 }
+
+export default TravelCardV3;
