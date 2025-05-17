19e2f2bdab4dcdbaed967fb508b6d5481962c55e
lorenzosalti <lorenzosalti@gmail.com>
2025-05-17 15:55:45 +0200

    add: user card component

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+
+function UserCard({ user }) {
+
+  return (
+    <Link to={`/users/${user.id}`} className="px-2">
+      <div class="col card">
+        <div className="card-body">
+          <h3>{user.firstName} {user.lastName}</h3>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default UserCard
