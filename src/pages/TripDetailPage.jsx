243aa00275ab6b015cf95eb678ba31b836c7c77d
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-18 17:14:56 +0200

    feat: add filter for user

--- src/pages/TripDetailPage.jsx
+++ src/pages/TripDetailPage.jsx
@@ -4,13 +4,37 @@ import { TourOperatorCard } from "../components/TourOperatorCard.jsx";
 
 import userDataDemo from "../data/userDataDemo"
 import UserCard from "../components/UserCard.jsx";
+import { useEffect, useState } from "react";
 
 // Pagina di dettaglio viaggio
 function TripDetailPage() {
   const { id } = useParams();
 
-  // filteriing user by id
-  const tripUsers = userDataDemo.filter(user => user.tripId == id)
+  const [user, setUser] = useState(userDataDemo);
+  const [findUser, setFindUser] = useState(user);
+  const [firstNameSearchUser, setFirstNameSearchUser] = useState('');
+  const [lastNameSearchUser, setLastNameSearchUser] = useState('');
+
+  useEffect(() => {
+
+    let filteredUser = user;
+
+    if(firstNameSearchUser !== '') {
+
+      filteredUser = filteredUser.filter(user => user.firstName.toLowerCase().includes(firstNameSearchUser.toLowerCase()));
+    }
+
+    if(lastNameSearchUser !== '') {
+
+      filteredUser = filteredUser.filter(user => user.lastName.toLowerCase().includes(lastNameSearchUser.toLowerCase()));
+    }
+
+    setFindUser(filteredUser);
+
+  }, [firstNameSearchUser, lastNameSearchUser]);
+
+  // filtering user by id
+  const tripUsers = findUser.filter(user => user.tripId == id);
 
   return (
     <>
@@ -24,9 +48,18 @@ function TripDetailPage() {
           Hai selezionato il viaggio con ID: <strong>{id}</strong>
         </p>
 
+        <div className="row row-cols-2 mb-4">
+          <div className="col">
+            <input className="form-control me-2" type="search" placeholder="Search By First Name" value={firstNameSearchUser} onChange={e => setFirstNameSearchUser(e.target.value)} />
+          </div>
+          <div className="col">
+            <input className="form-control me-2" type="search" placeholder="Search By Last Name" value={lastNameSearchUser} onChange={e => setLastNameSearchUser(e.target.value)} />
+          </div>
+        </div>
+        
         <section className="container text-center">
           <div className="row row-cols-3 g-3">
-            {tripUsers.map(user => <UserCard user={user} />)}
+            {tripUsers.map(user => <UserCard key={user.id} user={user} />)}
           </div>
         </section>
 
