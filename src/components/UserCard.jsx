f948e0eab583698734885a24c207800485a574a8
lorenzosalti <lorenzosalti@gmail.com>
2025-05-19 18:06:58 +0200

    feat: user button style

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -5,7 +5,7 @@ function UserCard({ user }) {
 
   return (
     <Link to={`/users/${user.id}`} className="col col-12 col-md-6 col-lg-4 text-decoration-none">
-      <div className="card">
+      <div className="card btn btn-light shadow-sm p-3 mb-2">
         <div className="card-body">
           <h3>{user.firstName} {user.lastName}</h3>
         </div>
