4fb37177367c1ddffc5d415a8d383f919bbe75f9
lorenzosalti <lorenzosalti@gmail.com>
2025-05-19 16:53:15 +0200

    feat: users grid responsive to small devices

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function UserCard({ user }) {
 
   return (
-    <Link to={`/users/${user.id}`} className="col col-6 col-lg-4 text-decoration-none">
+    <Link to={`/users/${user.id}`} className="col col-12 col-md-6 col-lg-4 text-decoration-none">
       <div className="card">
         <div className="card-body">
           <h3>{user.firstName} {user.lastName}</h3>
